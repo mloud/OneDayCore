f6ace17998bd9b5fba7524ad538284c306ea5644
Michal Louda <michal.louda@chargedmonkey.com>
2022-04-18 07:09:53 +0200

    extensions

---
+++ AppState/Assets/Core/Scripts/Extensions/ContainerExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class ContainerExtensions
+{
+    public static T GetRandomElement<T>(this IEnumerable<T> enumerable)
+    {
+        int index = UnityEngine.Random.Range(0, enumerable.Count());
+        return enumerable.ElementAt(index);
+    }
+}
---
+++ AppState/Assets/Core/Scripts/Extensions/LockCameraPosition.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Cinemachine;
+ 
+/// <summary>
+/// An add-on module for Cinemachine Virtual Camera that locks the camera's Y co-ordinate
+/// </summary>
+[SaveDuringPlay] [AddComponentMenu("")] // Hide in menu
+[ExecuteInEditMode]
+public class LockCameraPosition : CinemachineExtension
+{
+    [Tooltip("Lock the camera's X position to this value")]
+    public float m_XPosition = 0;
+
+    public bool m_LockXPosition = false;
+    
+    [Tooltip("Lock the camera's Y position to this value")]
+    public float m_YPosition = 0;
+
+    public bool m_LockYPosition = false;
+    
+    protected override void PostPipelineStageCallback(
+        CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            var pos = state.RawPosition;
+            if (m_LockXPosition)
+            {
+                pos.x = m_XPosition;
+            }
+
+            if (m_LockYPosition)
+            {
+                pos.y = m_YPosition;
+            }
+
+            state.RawPosition = pos;
+        }
+    }
+}
